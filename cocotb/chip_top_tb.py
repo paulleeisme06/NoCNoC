7dd99383ed4001f2a448b485fb63be38c5f8159d
Paul Lee <flee14@ucsc.edu>
2026-05-11 12:01:39 -0700

    fix

--- cocotb/chip_top_tb.py
+++ cocotb/chip_top_tb.py
@@ -120,10 +120,10 @@ def chip_top_runner():
         # Ethan: Added these files:
         proj_path / "../src/dft - Ethan/spi_debug.v",
         proj_path / "../src/dft - Ethan/gf180mcu_fd_ip_sram__sram2048x8m8wm1.v",
-        proj_path / "../src/mesh - Psi&Aan/mesh_3x3.v",
-        proj_path / "../src/mesh - Psi&Aan/mesh_tile.v",
-        proj_path / "../src/mesh - Psi&Aan/mesh_router.v",
-        proj_path / "../src/mesh - Psi&Aan/boot_controller.v",
+        proj_path / "../src/mesh_psi_aan/mesh_3x3.v",
+        proj_path / "../src/mesh_psi_aan/mesh_tile.v",
+        proj_path / "../src/mesh_psi_aan/mesh_router.v",
+        proj_path / "../src/mesh_psi_aan/boot_controller.v",
         # Add all .v files from subservient and serv submodules:
         *sorted((proj_path / "../src/subservient/rtl").glob("*.v")),
         *sorted((proj_path / "../src/serv/rtl").glob("*.v")),
--- src/flash_Sumi/README.md
+++ src/flash_Sumi/README.md
@@ -3,7 +3,7 @@ To run the test run:
 
 //This makes a symlink for both folders before testing
 cd /mnt/c/Users/mukte/Documents/CSE127/NoCNoC
-ln -s "mesh - Psi&Aan" src/mesh
+ln -s "mesh_psi_aan" src/mesh
 ln -s "dft - Ethan" src/dft
 ls src/mesh/
 ls src/dft/
